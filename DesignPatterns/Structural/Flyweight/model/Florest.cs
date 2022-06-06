810390915c0d99d5979d1b52cc17ee07c5603000
MateusSantosF <mateusferreira.dev@gmail.com>
2022-06-06 12:43:00 -0300

    Document Flyweight

--- DesignPatterns/Structural/Flyweight/model/Florest.cs
+++ DesignPatterns/Structural/Flyweight/model/Florest.cs
@@ -11,6 +11,13 @@ namespace DesignPatterns.Structural.Flyweight.model
 
         public List<Tree> Trees { get; set; }
 
+        /// <summary>
+        /// Utilize o padrão Flyweight apenas quando seu programa deve suportar um grande número 
+        /// de objetos que mal cabem na RAM disponível.
+        /// <para/>
+        /// A ideia é separar propriedades imutaveis entre objetos que serão criados em grande número em seu programa
+        /// assim, guardando a referência das propriedades imutaveis para replicar sempre que um novo objeto é criado.
+        /// </summary>
         public Florest(){
             Trees = new List<Tree>();
         }
